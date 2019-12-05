af669117642eb5d72a1b145ddfc797e25e08f508
Michael Huang <mhuang19@cs.washington.edu>
2019-12-05 12:05:39 -0500

    updated some writing

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Short INFO 201A extra credit assignment!
 <hr>
 Github Repository: https://github.com/michaelghuang19/INFO-aX
 <br>
-Report URL: https://michaelghuang19.github.io/INFO-aX
+Report URL: https://michaelghuang19.github.io/INFO-aX/index.html
--- analysis.R
+++ analysis.R
@@ -2,6 +2,10 @@ library(dplyr)
 library(data.table)
 library(plotly)
 library(ggplot2)
+setwd("~/uw19.20/2019.9/info201/workspace/INFO-aX")
 
 
+df <- read.csv("data/master.csv")
+
+# we can group_by, or anything else since this is very fluid notebook.
 
